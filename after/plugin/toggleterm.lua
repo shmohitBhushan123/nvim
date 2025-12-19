1f74226972d3c5c5aa9241c74161f181fc0ea86f
Mohit Bhushan <mbhushan123@yahoo.com>
2025-12-19 16:56:47 -0500

    added git ingnore and changed toggle term mapping

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -5,7 +5,7 @@ require("toggleterm").setup({
     width = math.floor(vim.o.columns * 0.8),
     height = math.floor(vim.o.lines * 0.8),
   },
-  open_mapping = [[<leader>tm]],
+  open_mapping = [[<leader>tt]],
 })
 
 -- Add escape key mapping to close toggleterm
